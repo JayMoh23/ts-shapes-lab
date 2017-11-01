2281b42f3e17133037340dcba23f4891ccf70773
jaymoh23 <jeffreyryanmurray@gmail.com>
2017-11-01 09:44:35 -0500

    work

--- script.ts
+++ script.ts
@@ -1,4 +1,22 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const shapeContainer=document.getElementsByClassName('main-div');
+    const squareButton=document.getElementById("square-button");
+    const rectangleButton=document.getElementById("rectangle-button");
+    const circleButton=document.getElementById("circle-button");
+    const triangleButton=document.getElementById("triangle-button");
+    
+    const squareInput=document.getElementById("square-input");
+    const rectangleInput=document.getElementById("rectangle-input");
+    const circleInput=document.getElementById("circle-input");
+    const triangleInput=document.getElementById("triangle-input");
+    
+    
+})
 class Shape {
+    div:HTMLDivElement;
+    constructor() {
+       
+    }
     
 }
 
